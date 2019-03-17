2e38baf5c1f4a8fa8e8d2568336e5cd06fa4a6f1
Radoslaw Biernacki <radoslaw.biernacki@gmail.com>
2019-03-17 20:39:45 +0100

    Switching to public Word Frequency database

--- README.md
+++ README.md
@@ -1,35 +1,37 @@
 # Passphrase entropy checker
 
-This simple phrase password checker allows you to estimate the entropy of your
-password.
+This simple passphrase entropy checker allows you to estimate the entropy in
+case your passphrase is created by conjuntion of several words.
 
 # Idea
 
-The idea behind this tool is that new password recommendations starting to
-suggest that old rules that force user to use set of characters and certain
-length in password, creates password that are hard to remember and easy to
-crack. 
+The idea behind this tool is to verify the real entropy of passphrase based on
+several words stiched toghether. 
 
-Those recommendations suggest to just use long password like in form of several
+In the past companies followed NIST recomendations that force users to use set
+of characters and certain length in password, which creates password that are
+hard to remember and easy to crack. 
+
+New NIST recommendations suggest to just use long password like in form of several
 words connected together. The idea behind this is that such password is easy to
 remember and hard to crack.
 
-The cracking resistance comes from much bigger key space. Lest calculate:
+The cracking resistance comes from much bigger key space.
 
 8 character password with base set of [a-z][A-Z][0-9][_?!@#$%^&*()]:
 
-Entropy = 78^8 = 1.3e15
+Key space = 78^8 = 1.3e15
 
 Password made from 5 words (lets assume 5000 word set)
 
-Entropy = 5000^5 = 3.1e18
+Key space = 5000^5 = 3.1e18
 
-But there is a caveat in those calculations. We assumed that all words came
-certain set, which in real life does not have to be true. User have the tendency
-to use common words which create much smaller set.
-
-With smaller word set the entropy drastically drops. And that's why it is important
-to verify how common are words used in your password.
+But there is a caveat in those calculations. We assumed that all words came from
+certain set, which in real life does not have to be large as in our
+calculations. Users have the tendency to use common words from much smaller set.
+In that case the entropy drops dramaticly.  And that's why it is important to
+verify how common are words used in your password. And that's why this tool is
+handy.
 
 Some may disagree that using "leetspeek" or mixing capital letters make small word
 set attack impractical but in fact, using those tricks only increase the set by
@@ -38,13 +40,17 @@ dictionary + ruleset attach in common cracking tools like hashcat.
 
 # Usage
 
-Just start the tool with "python pass_check.py" and give your words one by one
-pytting attention to proper spelling.
-In future some mechanism for fixing spelling and "letsspeek" demangling is planed
-to be implemented.
+Just start the tool with "./pass_check.py" with your international language code
+(like 'en' for english). Tool will download proper word frequency list. Than you
+will be asked to give words which are elements of your password, one by one
+pytting attention to proper spelling. This is important as tool does not
+currently is not able to scan whole passphrase to look up for words so you need
+to just gave those words with proper spelling.
+In future some mechanism for automatic scanning, fixing spelling and "letsspeek"
+demangling is planed to be implemented.
 
 # Languages
 
-Currently only Polish language is supported. But any language can be added just
-by importing frequency word list database.
+Thanks to public Frequency Words database on CC license, you can use any
+language of your choice. 
 
--- pass_check.py
+++ pass_check.py
@@ -5,95 +5,120 @@ import math
 import operator
 import os  
 import signal
+import random
+import sys, getopt
 
 def signal_handler(sig, frame):
         print('SIGINT ... exiting')
         sys.exit(0)
 
-signal.signal(signal.SIGINT, signal_handler)
+def main(argv):
 
-if not os.path.exists('frequency_list_orth.txt'):
-   print 'Word list file not found. Extracting archive ...'
-   os.system('7z e frequency_list_orth.7z') #do you often run python script without review?
+   freq_list_url = 'https://raw.githubusercontent.com/hermitdave/FrequencyWords/master/content/2018'
+   lang = 'pl'
 
-print 'Loading words (this can take a while) ...'
-words = [] #list
-words_filter= {}
-with open('frequency_list_orth.txt', 'r') as f:
-   for line in f:
-      try:
-         subs = line.strip().split(';')
-         if subs[2] not in words_filter:
-            words.append((subs[2], int(subs[3])))
-            words_filter[subs[2]] = 1
-      except ValueError:
-         continue #ignoring lines with headers and comments
+   try:
+      opts, args = getopt.getopt(argv,"l:",["lang="])
+   except getopt.GetoptError:
+      print './pass_check.py [-l LANG_CODE]'
+      sys.exit(2)
+   for opt, argval in opts:
+      if opt in ("-l", "--lang="):
+         print 'Got lang %s' % argval
+         lang = argval
+      else:
+         print './pass_check.py [-l LANG_CODE]'
+         sys.exit(2)
 
-print '%d words loaded' % len(words)
+   freq_list_file = '%s_full.txt' % lang
 
-print 'Generating freqs ...'
-pos = 0
-cum = 1
-prev_count = 0
-freq = {}
-for word in words:
-   count = word[1]
-   if (prev_count != count):
-      pos += cum
-      cum = 1
-   else:
-      cum += 1
-   freq[count] = pos
+   if not os.path.exists(freq_list_file):
+      print 'Downloading word frequency list ...'
+      os.system('wget %s/%s/%s' % (freq_list_url, lang, freq_list_file)) #do you often run python script without review?
+
+   signal.signal(signal.SIGINT, signal_handler)
+
+   print 'Loading words (this can take a while) ...'
+   words = [] #list
+   words_filter= {}
+   with open(freq_list_file, 'r') as f:
+      for line in f:
+         try:
+            subs = line.strip().split(' ')
+            if subs[0] not in words_filter:
+               words.append((subs[0], int(subs[1])))
+               words_filter[subs[0]] = 1
+         except ValueError:
+            continue #ignoring lines with headers and comments
+
+   print '%d words loaded' % len(words)
+
+   print 'Generating freqs ...'
+   pos = 0
+   cum = 1
+   prev_count = 0
+   freq = {}
+   for word in words:
+      count = word[1]
+      if (prev_count != count):
+         pos += cum
+         cum = 1
+      else:
+         cum += 1
+      freq[count] = pos
 
 #print freq
 
-score = {}
-for word in words:
-   seq = (word[1], freq[word[1]])
-   score[word[0]] = seq
+   score = {}
+   for word in words:
+      seq = (word[1], freq[word[1]])
+      score[word[0]] = seq
 
 #print score
 
-print 'Enter each word from your password, one at the time with proper spelling (important)'
-print 'Example:\n> chrząszcz\n> brzebrzydły\n> moczymorda'
-print 'Enter empty line to reset calculations'
-set_size = 1
-word_cnt = 0
-pass_len = 0
-while True:
-   print '\n> ',
-   word = sys.stdin.readline().strip()
-   if word == '':
-      set_size = 1
-      word_cnt = 0
-      pass_len = 0
-      print 'Reseting calculations. Clean start'
-      continue
+   print 'Enter each word from your password, one at the time with proper spelling (important)'
+   print 'Example:\n> chrząszcz\n> brzebrzydły\n> moczymorda'
+   print 'Enter empty line to reset calculations'
+   set_size = 1
+   word_cnt = 0
+   pass_len = 0
+   while True:
+      print '\n> ',
+      word = sys.stdin.readline().strip()
+      if word == '':
+         set_size = 1
+         word_cnt = 0
+         pass_len = 0
+         print 'Reseting calculations. Clean start'
+         continue
 
-   word_cnt += 1
-   pass_len += len(word)
-   if word not in score:
-      print 'Your word seems to be uncomon or misspelled, cannot find the score (password uncracable? check haveibeenpwned)'
-      print 'Assuming whole word set as crack set, %d words' % len(score)
-      set_size = max(set_size, len(score))
-   else:
-      hit = score[word]
-      set_size = max(set_size, hit[1])
-      print '\"%s\" rank #%d used %d times, minimal word set size %d' % (word,hit[1],hit[0],set_size)
+      word_cnt += 1
+      pass_len += len(word)
+      if word not in score:
+         print 'Your word seems to be uncomon or misspelled, cannot find the score (password uncracable? check haveibeenpwned)'
+         print 'Assuming whole word set as crack set, %d words' % len(score)
+         set_size = max(set_size, len(score))
+      else:
+         hit = score[word]
+         set_size = max(set_size, hit[1])
+         print '\"%s\" rank #%d used %d times, minimal word set size %d' % (word,hit[1],hit[0],set_size)
+         
+      word_perm = pow(set_size, word_cnt)
+      brute_perm = pow(26, pass_len)
       
-   word_perm = pow(set_size, word_cnt)
-   brute_perm = pow(26, pass_len)
-   
-   print '%d word password entropy %f bits' % (word_cnt, math.log(float(word_perm),2))
-   print '%d letters bruteforce password entropy %f bits' % (pass_len, math.log(float(brute_perm),2))
-   seconds = operator.truediv(min(word_perm, brute_perm),23012000000) #magic number is hashrate of 8 Nvidia 1080 for sha256
-   if seconds < 60:
-      print 'crackable in %d seconds' % seconds
-   elif seconds / 60 < 60:
-      print 'crackable in %d minutes' % (seconds/60)
-   elif seconds / 3600 < 24:
-      print 'crackable in %d hours' % (seconds/3600)
-   elif seconds / (3600 * 24) < 365:
-      print 'crackable in %d days' % (seconds / (3600 * 24))
-   else: 
-      print 'crackable in %d years' % (seconds / (3600 * 24 * 365))
+      print '%d word password entropy %f bits' % (word_cnt, math.log(float(word_perm),2))
+      print '%d letters bruteforce password entropy %f bits' % (pass_len, math.log(float(brute_perm),2))
+      seconds = operator.truediv(min(word_perm, brute_perm),23012000000) #magic number is hashrate of 8 Nvidia 1080 for sha256
+      if seconds < 60:
+         print 'crackable in %d seconds' % seconds
+      elif seconds / 60 < 60:
+         print 'crackable in %d minutes' % (seconds/60)
+      elif seconds / 3600 < 24:
+         print 'crackable in %d hours' % (seconds/3600)
+      elif seconds / (3600 * 24) < 365:
+         print 'crackable in %d days' % (seconds / (3600 * 24))
+      else: 
+         print 'crackable in %d years' % (seconds / (3600 * 24 * 365))
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
